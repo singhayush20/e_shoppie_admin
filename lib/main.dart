9eab4c7bf06e100f7b374152b4675bc15cb47a00
singhayush20 <ayushsingh20november@gmail.com>
2022-08-24 16:53:46 +0530

    import statement deleted

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:e_shoppie_admin/screens/admin_home.dart';
-import 'package:e_shoppie_admin/screens/admin_screen.dart';
 import 'package:e_shoppie_admin/themes.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
